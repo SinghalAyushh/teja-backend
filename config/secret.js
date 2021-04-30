e81c98da97bc093dd2f27f3fb10c210eded80579
ayush <aayusinghal34@gmail.com>
2021-04-30 14:38:35 +0530

    Initial commit

---
+++ config/secret.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    database: "mongodb+srv://Ayush:"+encodeURIComponent("Django@123")+"@cluster0.87gle.mongodb.net/math?retryWrites=true&w=majority",
+    secret: '$43@#asb)9!~asd&^%'
+};
